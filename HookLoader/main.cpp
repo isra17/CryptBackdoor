de0726f20870f0b4682be98822013d62da27c696
isra17 <isra017@gmail.com>
2015-10-17 17:44:27 -0400

    Fix loader

--- HookLoader/main.cpp
+++ HookLoader/main.cpp
@@ -11,45 +11,14 @@ using namespace std;
 namespace filesystem = std::experimental::filesystem;
 
 const string kUsage = "Usage: HookLoader [-p pid] [-n name] [pids...] hooked_dll";
-const wstring kKernel32 = L"kernel32.dll";
-
-
-struct ProcessInfo {
-	DWORD pid;
-	wstring exeFile;
-	HMODULE hKernel32;
-};
 
 wstring utf8towcs(const string& utf8) {
 	std::wstring_convert<codecvt<wchar_t, char, mbstate_t>> converter;
 	return converter.from_bytes(utf8);
 }
 
-
-ProcessInfo ProcessInfoFromPid(int pid) {
-	ProcessInfo process;
-	process.pid = pid;
-
-	HANDLE hTool32 = CreateToolhelp32Snapshot(TH32CS_SNAPMODULE, pid);
-	MODULEENTRY32 me32;
-	me32.dwSize = sizeof(MODULEENTRY32);
-	BOOL bModule = Module32First(hTool32, &me32);
-	if (bModule == TRUE)
-	{
-		process.exeFile = me32.szExePath;
-		while ((Module32Next(hTool32, &me32)) == TRUE)
-			if (kKernel32 == me32.szExePath) {
-				process.hKernel32 = me32.hModule;
-			}
-	}
-	CloseHandle(hTool32);
-
-	return process;
-}
-
-
-vector<ProcessInfo> FindProcesses(const wstring& processName) {
-	vector<ProcessInfo> processes;
+vector<int> FindProcessesPid(const wstring& processName) {
+	vector<int> pids;
 	PROCESSENTRY32 pe32;
 	pe32.dwSize = sizeof(PROCESSENTRY32);
 	HANDLE hTool32 = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
@@ -58,21 +27,20 @@ vector<ProcessInfo> FindProcesses(const wstring& processName) {
 	{
 		while ((Process32Next(hTool32, &pe32)) == TRUE)
 			if (processName == pe32.szExeFile) {
-				processes.push_back(ProcessInfoFromPid(pe32.th32ProcessID));
+				pids.push_back(pe32.th32ProcessID);
 			}
 	}
 	CloseHandle(hTool32);
 
-	return processes;
+	return pids;
 }
 
 bool IsFlag(const string& opt) {
 	return opt[0] == '-';
 }
 
-
 struct Options {
-	vector<ProcessInfo> processes;
+	vector<int> pids;
 	filesystem::path hookedDllPath;
 
 	static Options Parse(int argc, char** argv) {
@@ -102,12 +70,12 @@ struct Options {
 
 				switch (arg[1]) {
 				case 'p':
-					options.processes.push_back(ProcessInfoFromPid(atoi(parg[1])));
+					options.pids.push_back(atoi(parg[1]));
 					break;
 				case 'n':
 				{
-					vector<ProcessInfo> processes = FindProcesses(utf8towcs(parg[1]));
-					options.processes.insert(options.processes.end(), processes.begin(), processes.end());
+					vector<int> pids = FindProcessesPid(utf8towcs(parg[1]));
+					options.pids.insert(options.pids.end(), pids.begin(), pids.end());
 				}
 					break;
 				default:
@@ -119,7 +87,7 @@ struct Options {
 				parg += 2;
 			}
 			else {
-				options.processes.push_back(ProcessInfoFromPid(atoi(arg)));
+				options.pids.push_back(atoi(arg));
 				parg += 1;
 			}
 		}
@@ -129,11 +97,11 @@ struct Options {
 
 };
 
-void HookProcess(const ProcessInfo& process, const wstring& hookedDllPath) {
+void HookProcess(int pid, const wstring& hookedDllPath) {
 	HANDLE hProcess = OpenProcess(
 		PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION | PROCESS_VM_WRITE |
-		PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, false, process.pid);
-	LPVOID pLoadLibraryW = GetProcAddress(process.hKernel32, "LoadLibraryW");
+		PROCESS_QUERY_INFORMATION | PROCESS_VM_READ , false, pid);
+	LPVOID pLoadLibraryW = GetProcAddress(GetModuleHandle(L"kernel32.dll"), "LoadLibraryW");
 	DWORD pathSize = (hookedDllPath.length() + 1) * 2;
 	LPVOID lpParam = (LPVOID)VirtualAllocEx(hProcess, NULL, pathSize, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
 	if (!WriteProcessMemory(hProcess, lpParam, hookedDllPath.c_str(), pathSize, NULL)) {
@@ -141,15 +109,15 @@ void HookProcess(const ProcessInfo& process, const wstring& hookedDllPath) {
 	}
 
 	HANDLE hTh = CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)pLoadLibraryW, lpParam, NULL, NULL);
-	wcout << "Hooked '" << hookedDllPath << "' in process " << process.exeFile << " [" << hProcess << "] from thread [" << hTh << "]" << endl;
+	wcout << "Hooked '" << hookedDllPath << "' in process [" << hProcess << "] from thread [" << hTh << "]" << endl;
 	CloseHandle(hProcess);
 }
 
 int main(int argc, char* argv[]) {
 	Options options = Options::Parse(argc, argv);
 
-	for (auto process : options.processes) {
-		HookProcess(process, options.hookedDllPath);
+	for (int pid : options.pids) {
+		HookProcess(pid, options.hookedDllPath);
 	}
 
 	getchar();
